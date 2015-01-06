fb824eb5e95b7854ddc1d6a367dc375a37142219
Mick RAVOAVY <mravoavy@e2r1p12.42.fr>
2015-01-06 21:52:16 +0100

    changed caching

--- start/server.js
+++ start/server.js
@@ -8,12 +8,32 @@ var mimeTypes= {
     '.css' : 'text/css'
 };
 
+var cache = {};
+function cacheAndDeliver (f, cb){
+    fs.stat(f, function(err, stats){
+       if (err){return console.log('Oh noooo!, erreeur error');}
+        var lastChanged = Date.parse(stats.ctime),
+            isUpdated = (cache[f]) && lastChanged > cache[f].timeStamp;
+        if (!cache[f] || isUpdated){
+            fs.readFile(f, function(err, data){
+                console.log('loading' + f + ' from cache');
+                if (!err){
+                    cache[f] = {content: data};
+                }
+                cb(err, data);
+            });
+            return;
+        }
+        cb(null, cache[f].content);
+    });
+}
+
 var server = http.createServer(function(req, res){
     var lookup = path.basename (decodeURI(req.url)) || 'index.html';
     var f = 'content/' + lookup;
     fs.exists(f, function(exists){
        if (exists){
-           fs.readFile(f, function(err, data){
+           cacheAndDeliver(f, function(err, data){
                if (err){
                    res.writeHead(500); res.end('Server error');
                    return;
@@ -25,7 +45,7 @@ var server = http.createServer(function(req, res){
            return;
        }
         res.writeHead(404);
-        res.end();
+        res.end("page not found");
     });
 
 });
