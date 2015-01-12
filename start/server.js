73b36d97e057618956d90152c64543cc76b8bb01
Mick RAVOAVY <mravoavy@e2r2p19.42.fr>
2015-01-12 16:51:41 +0100

    with stream

--- start/server.js
+++ start/server.js
@@ -9,45 +9,62 @@ var mimeTypes= {
 };
 
 var cache = {};
-function cacheAndDeliver (f, cb){
-    fs.stat(f, function(err, stats){
-       if (err){return console.log('Oh noooo!, erreeur error');}
-        var lastChanged = Date.parse(stats.ctime),
-            isUpdated = (cache[f]) && lastChanged > cache[f].timeStamp;
-        if (!cache[f] || isUpdated){
-            fs.readFile(f, function(err, data){
-                console.log('loading' + f + ' from cache');
-                if (!err){
-                    cache[f] = {content: data};
-                }
-                cb(err, data);
-            });
-            return;
-        }
-        cb(null, cache[f].content);
-    });
-}
 
+//function cacheAndDeliver (f, cb){
+//    fs.stat(f, function(err, stats){
+//       if (err){return console.log('Oh noooo!, erreeur error');}
+//        var lastChanged = Date.parse(stats.ctime),
+//            isUpdated = (cache[f]) && lastChanged > cache[f].timeStamp;
+//        if (!cache[f] || isUpdated){
+//            fs.readFile(f, function(err, data){
+//                console.log('loading' + f + ' from cache');
+//                if (!err){
+//                    cache[f] = {content: data};
+//                }
+//                cb(err, data);
+//            });
+//            return;
+//        }
+//        cb(null, cache[f].content);
+//    });
+//}
 
 var server = http.createServer(function(req, res){
     var lookup = path.basename (decodeURI(req.url)) || 'index.html';
     var f = 'content/' + lookup;
     fs.exists(f, function(exists){
        if (exists){
-           cacheAndDeliver(f, function(err, data){
-               if (err){
-                   res.writeHead(500); res.end('Server error');
-                   return;
-               }
                var headers = {'Content-Type' : mimeTypes[path.extname(lookup)]};
+           if (cache[f]) {
                res.writeHead(200, headers);
-               res.end(data);
+               res.end(cache[f].content);
+               return;
+            }
+           var s = fs.createReadStream(f).once('open', function(){
+
+               res.writeHead(200, headers);
+               this.pipe(res);
+           }).once('error', function(e){
+               console.log(e);
+               res.writeHead(500);
+               res.end('Server Error!');
            });
+
+           fs.stat(f, function(err, stats){
+               var bufferOffSet = 0;
+               cache[f] = {content: new Buffer(stats.size)};
+               s.on('data', function (chunk){
+                   chunk.copy(cache[f].content,bufferOffSet);
+                   bufferOffSet += chunk.length;
+               });
+           });
+
            return;
-       }
+    }
         res.writeHead(404);
         res.end("page not found");
     });
 
 });
+
 server.listen(8000);
